b79da35171015307c434c5fee0eb77cfcb79c6f9
victor orlunda <vicorlunda@gmail.com>
2023-11-15 07:48:16 +0100

    add bash script for generating candid files

---
+++ did.sh
@@ -0,0 +1,18 @@
+
+function generate_did() {
+  local canister=$1
+  canister_root="./$canister"
+
+  cargo build --manifest-path="$canister_root/Cargo.toml" \
+      --target wasm32-unknown-unknown \
+      --release --package "$canister" \
+
+  candid-extractor "target/wasm32-unknown-unknown/release/$canister.wasm" > "$canister_root/$canister.did"
+}
+
+CANISTERS=ems_backend
+
+for canister in $(echo $CANISTERS | sed "s/,/ /g")
+do
+    generate_did "$canister"
+done
